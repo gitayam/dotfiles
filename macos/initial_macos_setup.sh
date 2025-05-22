03019cdd62c10bc37b1e2f9208061936c36a67bf
github.ayam <github.ayam@slmail.me>
2025-05-22 18:22:08 -0400

    include ImageMagick in terminal applications installation list

--- macos/initial_macos_setup.sh
+++ macos/initial_macos_setup.sh
@@ -13,7 +13,7 @@ fi
 
 # Loop through this list checking if the application is installed and if not then install it
 install_terminal_apps() {
-  terminal_apps=("coreutils" "diceware" "git" "gh" "tree" "python" "ansible" "docker" "kubectl" "gpg" "age" "magic-wormhole" "wireguard-tools" "ocrmypdf" "pandoc" "tesseract" "clamav" "ghostscript")
+  terminal_apps=("coreutils" "diceware" "git" "gh" "tree" "python" "ansible" "docker" "kubectl" "gpg" "age" "magic-wormhole" "wireguard-tools" "ocrmypdf" "pandoc" "tesseract" "clamav" "ghostscript" "imagemagick")
   for app in "${terminal_apps[@]}"; do
     # Capitalize the first letter of the app name
     capitalized_app="$(echo "${app:0:1}" | tr '[:lower:]' '[:upper:]')${app:1}"
